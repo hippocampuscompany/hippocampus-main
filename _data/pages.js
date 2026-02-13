fdddbc9f93433655c2430aa049ef71790905f596
Manjunath S <manjurshv@gmail.com>
2026-02-13 17:44:35 +0530

    static to dynamic

--- _data/pages.js
+++ _data/pages.js
@@ -8,8 +8,14 @@ const res = await fetch(
   "&populate[sections][on][page-sections.kbc-approach][populate]=*" +
   "&populate[sections][on][page-sections.kbc-slider][populate]=*" +
   "&populate[sections][on][page-sections.team-section][populate][members][populate]=image" +
-  "&populate[sections][on][page-sections.timeline][populate][timelineItems][populate]=image"
-);
+ "&populate[sections][on][page-sections.timeline][populate][timelineItems][populate]=image" +
+  "&populate[sections][on][page-sections.investor-section][populate][investors][populate]=image" +
+  "&populate[sections][on][page-sections.home-kbc][populate]=image" +
+ "&populate[sections][on][page-sections.home-slide][populate][slides][populate]=image" +
+  "&populate[sections][on][page-sections.school-slider][populate][schools][populate]=image"+
+  "&populate[sections][on][page-sections.stats-section][populate][stats][populate]=icon"
+  );
+
   const json = await res.json();
   return json.data;
 };
