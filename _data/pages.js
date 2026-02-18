8be4ac94ed5f8947d253042b822effc295bd8012
sowndaryahs8088 <sowndaryahsmca2025@gmail.com>
2026-02-18 16:04:28 +0530

    static to dynamic wall-of-dream

--- _data/pages.js
+++ _data/pages.js
@@ -19,9 +19,31 @@ const res = await fetch(
 "&populate[sections][on][page-sections.impact-image][populate][impactpoints]=*" +
 "&populate[sections][on][page-sections.cta-section][populate]=*" +
 "&populate[sections][on][page-sections.testimonial-slider][populate][testimonials][populate]=images" +
-"&populate[sections][on][page-sections.wall-of-dreams-hero][populate]=image"
+"&populate[sections][on][page-sections.wall-of-dreams-hero][populate]=image" +
+"&populate[sections][on][page-sections.gallery-section][populate][galleryItem][populate]=image" +
+"&populate[sections][on][page-sections.gallery-section][populate][program]=*" +
+"&populate[sections][on][page-sections.gallery-section][populate][school]=*"
 
 );
   const json = await res.json();
+  console.log(
+  json.data.map(p => p.slug)
+);
+  // Find hippocampus-main page
+const page = json.data.find(
+  p => p.slug === "wall-of-dreams"
+);
+
+if (!page) {
+  console.log("Page not found");
+} else {
+  console.log("Found Page:", page.title);
+
+  const gallerySection = page.sections.find(
+    s => s.__component === "page-sections.gallery-section"
+  );
+
+  console.log("Gallery Section:", gallerySection);
+}
   return json.data;
 };
